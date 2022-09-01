bc6b35a6f48b64c9294f6908c0d7283d147fcecd
Amina Hayat <hayat.amina@hotmail.com>
2022-09-01 15:30:50 +0100

    creang route

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,57 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newBlogPost  = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    
+    });
+    
+    res.status(200).json(newBlogPost );
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'Woops! Can not find a post with that ID!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:id", withAuth, async (req, res) => {
+    try {
+      const postData = await Post.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (!postData) {
+        res.status(404).json({ message: "Woops! Can not find a post with that ID!" });
+        return;
+      }
+      res.status(200).json(postData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+module.exports = router;
